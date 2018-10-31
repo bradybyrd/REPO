64771c513cb41f587e8013cdd1af38a26920a92b
Otto Mation <automation@dbmaestro.com>
2018-10-31 20:18:39 +0000

    Adding repository changes from automation - rlm v19 null

--- postgres/rlm/TABLE/locations.sql
+++ postgres/rlm/TABLE/locations.sql
@@ -9,7 +9,9 @@ CREATE TABLE public.locations (
     "COUNTRY_ID" character varying(255),
     watershed character varying(100),
     elevation integer,
-    rainfall integer
+    rainfall integer,
+    metro_area character varying(40),
+    metro_pop integer
 );
 ALTER TABLE public.locations OWNER TO deploy;
 --
