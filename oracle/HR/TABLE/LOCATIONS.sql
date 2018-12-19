c87f83c471d2695f592c95d9d57c0c45f2dfb419
Automation User <dbmdemo@dbmaestro.com>
2018-12-19 14:37:34 +0000

    DBm Hook update - HumanResources => DIT for version: V1.4.2

--- oracle/HR/TABLE/LOCATIONS.sql
+++ oracle/HR/TABLE/LOCATIONS.sql
@@ -1,4 +1,5 @@
-CREATE TABLE "HR"."LOCATIONS" 
+
+  CREATE TABLE "LOCATIONS" 
    (	"LOCATION_ID" NUMBER(4,0), 
 	"STREET_ADDRESS" VARCHAR2(40), 
 	"POSTAL_CODE" VARCHAR2(12), 
@@ -8,32 +9,27 @@ CREATE TABLE "HR"."LOCATIONS"
 	"PLANET" VARCHAR2(50), 
 	"REGION_ID" NUMBER, 
 	"SOLARSYSTEM" VARCHAR2(50), 
-	"ELEVATION" NUMBER
+	"ELEVATION" NUMBER, 
+	"OFFICE_DIRECTIONS" VARCHAR2(255)
    ) ;
-   COMMENT ON COLUMN "HR"."LOCATIONS"."LOCATION_ID" IS 'Primary key of locations table';
-   COMMENT ON COLUMN "HR"."LOCATIONS"."STREET_ADDRESS" IS 'Street address of an office, warehouse, or production site of a company.
+   COMMENT ON COLUMN "LOCATIONS"."LOCATION_ID" IS 'Primary key of locations table';
+   COMMENT ON COLUMN "LOCATIONS"."STREET_ADDRESS" IS 'Street address of an office, warehouse, or production site of a company.
 Contains building number and street name';
-   COMMENT ON COLUMN "HR"."LOCATIONS"."POSTAL_CODE" IS 'Postal code of the location of an office, warehouse, or production site
+   COMMENT ON COLUMN "LOCATIONS"."POSTAL_CODE" IS 'Postal code of the location of an office, warehouse, or production site
 of a company. ';
-   COMMENT ON COLUMN "HR"."LOCATIONS"."CITY" IS 'A not null column that shows city where an office, warehouse, or
+   COMMENT ON COLUMN "LOCATIONS"."CITY" IS 'A not null column that shows city where an office, warehouse, or
 production site of a company is located. ';
-   COMMENT ON COLUMN "HR"."LOCATIONS"."STATE_PROVINCE" IS 'State or Province where an office, warehouse, or production site of a
+   COMMENT ON COLUMN "LOCATIONS"."STATE_PROVINCE" IS 'State or Province where an office, warehouse, or production site of a
 company is located.';
-   COMMENT ON COLUMN "HR"."LOCATIONS"."COUNTRY_ID" IS 'Country where an office, warehouse, or production site of a company is
+   COMMENT ON COLUMN "LOCATIONS"."COUNTRY_ID" IS 'Country where an office, warehouse, or production site of a company is
 located. Foreign key to country_id column of the countries table.';
-   COMMENT ON TABLE "HR"."LOCATIONS"  IS 'Locations table that contains specific address of a specific office,
+   COMMENT ON TABLE "LOCATIONS"  IS 'Locations table that contains specific address of a specific office,
 warehouse, and/or production site of a company. Does not store addresses /
 locations of customers. Contains 23 rows; references with the
 departments and countries tables. ';
-
-  CREATE INDEX "HR"."LOC_CITY_IX" ON "HR"."LOCATIONS" ("CITY") 
-  ;
-  CREATE INDEX "HR"."LOC_COUNTRY_IX" ON "HR"."LOCATIONS" ("COUNTRY_ID") 
-  ;
-  CREATE INDEX "HR"."LOC_STATE_PROVINCE_IX" ON "HR"."LOCATIONS" ("STATE_PROVINCE") 
-  ; 
-  ALTER TABLE "HR"."LOCATIONS" ADD CONSTRAINT "LOC_C_ID_FK" FOREIGN KEY ("COUNTRY_ID")
-	  REFERENCES "HR"."COUNTRIES" ("COUNTRY_ID") ENABLE; 
-  ALTER TABLE "HR"."LOCATIONS" MODIFY ("CITY" CONSTRAINT "LOC_CITY_NN" NOT NULL ENABLE);
-  ALTER TABLE "HR"."LOCATIONS" ADD CONSTRAINT "LOC_ID_PK" PRIMARY KEY ("LOCATION_ID")
-  USING INDEX  ENABLE; 
+LOC_CITY_IXLOC_COUNTRY_IXLOC_STATE_PROVINCE_IX
+  ALTER TABLE "LOCATIONS" ADD CONSTRAINT "LOC_C_ID_FK" FOREIGN KEY ("COUNTRY_ID")
+	  REFERENCES "COUNTRIES" ("COUNTRY_ID") ENABLE;
+  ALTER TABLE "LOCATIONS" MODIFY ("CITY" CONSTRAINT "LOC_CITY_NN" NOT NULL ENABLE);
+  ALTER TABLE "LOCATIONS" ADD CONSTRAINT "LOC_ID_PK" PRIMARY KEY ("LOCATION_ID")
+  USING INDEX  ENABLE;
