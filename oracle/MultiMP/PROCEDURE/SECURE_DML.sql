99ff137536c664af39405963d915233f1ad4df09
TeamWork <bradyb@dbmaestro.com>
2019-06-03 16:28:32 -0700

    DBm Hook update - MPProj_A => DEV_MP for version: V0.1__Elevation

--- oracle/MultiMP/PROCEDURE/SECURE_DML.sql
+++ oracle/MultiMP/PROCEDURE/SECURE_DML.sql
@@ -1,11 +1,10 @@
-CREATE OR REPLACE PROCEDURE "MULTIUAT"."SECURE_DML" 
+CREATE OR REPLACE PROCEDURE "SECURE_DML" 
 IS
 BEGIN
   IF TO_CHAR (SYSDATE, 'HH24:MI') NOT BETWEEN '08:00' AND '18:00'
         OR TO_CHAR (SYSDATE, 'DY') IN ('SAT', 'SUN') THEN
 	RAISE_APPLICATION_ERROR (-20205,
-		'You may only make changes during normal office hours');
+		'You may only make changes during normal office hours, dummy');
   END IF;
-END secure_dml;
+END secure_dml
 /
-   
