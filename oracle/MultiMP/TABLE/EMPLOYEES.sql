f008d2af94a4ff163098321e65534b175ec1d53f
TeamWork <bradyb@dbmaestro.com>
2019-06-03 10:31:54 -0700

    Adding repository changes from automation - MPProj_B vnull null

---
+++ oracle/MultiMP/TABLE/EMPLOYEES.sql
@@ -0,0 +1,99 @@
+CREATE TABLE "MULTIDEV2"."EMPLOYEES" 
+   (	"EMPLOYEE_ID" NUMBER(6,0), 
+	"FIRST_NAME" VARCHAR2(20), 
+	"LAST_NAME" VARCHAR2(25), 
+	"EMAIL" VARCHAR2(25), 
+	"PHONE_NUMBER" VARCHAR2(20), 
+	"HIRE_DATE" DATE, 
+	"JOB_ID" VARCHAR2(10), 
+	"SALARY" NUMBER(8,2), 
+	"COMMISSION_PCT" NUMBER(2,2), 
+	"MANAGER_ID" NUMBER(6,0), 
+	"DEPARTMENT_ID" NUMBER(4,0)
+   ) ;
+
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."EMPLOYEE_ID" IS 'Primary key of employees table.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."FIRST_NAME" IS 'First name of the employee. A not null column.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."LAST_NAME" IS 'Last name of the employee. A not null column.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."EMAIL" IS 'Email id of the employee';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."PHONE_NUMBER" IS 'Phone number of the employee; includes country code and area code';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."HIRE_DATE" IS 'Date when the employee started on this job. A not null column.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."JOB_ID" IS 'Current job of the employee; foreign key to job_id column of the
+jobs table. A not null column.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."SALARY" IS 'Monthly salary of the employee. Must be greater
+than zero (enforced by constraint emp_salary_min)';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."COMMISSION_PCT" IS 'Commission percentage of the employee; Only employees in sales
+department elgible for commission percentage';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."MANAGER_ID" IS 'Manager id of the employee; has same domain as manager_id in
+departments table. Foreign key to employee_id column of employees table.
+(useful for reflexive joins and CONNECT BY query)';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."EMPLOYEES"."DEPARTMENT_ID" IS 'Department id where employee works; foreign key to department_id
+column of the departments table';
+ 
+   COMMENT ON TABLE "MULTIDEV2"."EMPLOYEES"  IS 'employees table. Contains 107 rows. References with departments,
+jobs, job_history tables. Contains a self reference.';
+ 
+
+  CREATE INDEX "MULTIDEV2"."EMP_DEPARTMENT_IX" ON "MULTIDEV2"."EMPLOYEES" ("DEPARTMENT_ID") 
+  ;
+ 
+  CREATE INDEX "MULTIDEV2"."EMP_JOB_IX" ON "MULTIDEV2"."EMPLOYEES" ("JOB_ID") 
+  ;
+ 
+  CREATE INDEX "MULTIDEV2"."EMP_MANAGER_IX" ON "MULTIDEV2"."EMPLOYEES" ("MANAGER_ID") 
+  ;
+ 
+  CREATE INDEX "MULTIDEV2"."EMP_NAME_IX" ON "MULTIDEV2"."EMPLOYEES" ("LAST_NAME", "FIRST_NAME") 
+  ;
+  
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_DEPT_FK" FOREIGN KEY ("DEPARTMENT_ID")
+	  REFERENCES "MULTIDEV2"."DEPARTMENTS" ("DEPARTMENT_ID") ENABLE;
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_JOB_FK" FOREIGN KEY ("JOB_ID")
+	  REFERENCES "MULTIDEV2"."JOBS" ("JOB_ID") ENABLE;
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_MANAGER_FK" FOREIGN KEY ("MANAGER_ID")
+	  REFERENCES "MULTIDEV2"."EMPLOYEES" ("EMPLOYEE_ID") ENABLE;
+  
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" MODIFY ("LAST_NAME" CONSTRAINT "EMP_LAST_NAME_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" MODIFY ("EMAIL" CONSTRAINT "EMP_EMAIL_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" MODIFY ("HIRE_DATE" CONSTRAINT "EMP_HIRE_DATE_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" MODIFY ("JOB_ID" CONSTRAINT "EMP_JOB_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_SALARY_MIN" CHECK (salary > 0) ENABLE;
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_EMP_ID_PK" PRIMARY KEY ("EMPLOYEE_ID") ENABLE;
+ 
+  ALTER TABLE "MULTIDEV2"."EMPLOYEES" ADD CONSTRAINT "EMP_EMAIL_UK" UNIQUE ("EMAIL") ENABLE;
+  
+  CREATE OR REPLACE TRIGGER "MULTIDEV2"."SECURE_EMPLOYEES" 
+  BEFORE INSERT OR UPDATE OR DELETE ON employees
+BEGIN
+  secure_dml;
+END secure_employees;
+/
+ALTER TRIGGER "MULTIDEV2"."SECURE_EMPLOYEES" DISABLE;
+ 
+  CREATE OR REPLACE TRIGGER "MULTIDEV2"."UPDATE_JOB_HISTORY" 
+  AFTER UPDATE OF job_id, department_id ON employees
+  FOR EACH ROW
+BEGIN
+  add_job_history(:old.employee_id, :old.hire_date, sysdate,
+                  :old.job_id, :old.department_id);
+END;
+/
+ALTER TRIGGER "MULTIDEV2"."UPDATE_JOB_HISTORY" ENABLE;
+ 
---
+++ oracle/MultiMP/TABLE/LOCATIONS.sql
@@ -0,0 +1,49 @@
+CREATE TABLE "MULTIDEV2"."LOCATIONS" 
+   (	"LOCATION_ID" NUMBER(4,0), 
+	"STREET_ADDRESS" VARCHAR2(40), 
+	"POSTAL_CODE" VARCHAR2(12), 
+	"CITY" VARCHAR2(30), 
+	"STATE_PROVINCE" VARCHAR2(25), 
+	"COUNTRY_ID" CHAR(2), 
+	"REGION_ID" NUMBER
+   ) ;
+
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."LOCATION_ID" IS 'Primary key of locations table';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."STREET_ADDRESS" IS 'Street address of an office, warehouse, or production site of a company.
+Contains building number and street name';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."POSTAL_CODE" IS 'Postal code of the location of an office, warehouse, or production site
+of a company. ';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."CITY" IS 'A not null column that shows city where an office, warehouse, or
+production site of a company is located. ';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."STATE_PROVINCE" IS 'State or Province where an office, warehouse, or production site of a
+company is located.';
+ 
+   COMMENT ON COLUMN "MULTIDEV2"."LOCATIONS"."COUNTRY_ID" IS 'Country where an office, warehouse, or production site of a company is
+located. Foreign key to country_id column of the countries table.';
+ 
+   COMMENT ON TABLE "MULTIDEV2"."LOCATIONS"  IS 'Locations table that contains specific address of a specific office,
+warehouse, and/or production site of a company. Does not store addresses /
+locations of customers. Contains 23 rows; references with the
+departments and countries tables. ';
+ 
+
+  CREATE INDEX "MULTIDEV2"."LOC_CITY_IX" ON "MULTIDEV2"."LOCATIONS" ("CITY") 
+  ;
+ 
+  CREATE INDEX "MULTIDEV2"."LOC_COUNTRY_IX" ON "MULTIDEV2"."LOCATIONS" ("COUNTRY_ID") 
+  ;
+ 
+  CREATE INDEX "MULTIDEV2"."LOC_STATE_PROVINCE_IX" ON "MULTIDEV2"."LOCATIONS" ("STATE_PROVINCE") 
+  ;
+  
+  ALTER TABLE "MULTIDEV2"."LOCATIONS" ADD CONSTRAINT "LOC_C_ID_FK" FOREIGN KEY ("COUNTRY_ID")
+	  REFERENCES "MULTIDEV2"."COUNTRIES" ("COUNTRY_ID") ENABLE;
+  
+  ALTER TABLE "MULTIDEV2"."LOCATIONS" MODIFY ("CITY" CONSTRAINT "LOC_CITY_NN" NOT NULL ENABLE);
+ 
+  ALTER TABLE "MULTIDEV2"."LOCATIONS" ADD CONSTRAINT "LOC_ID_PK" PRIMARY KEY ("LOCATION_ID") ENABLE;
+  
