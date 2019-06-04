9fb8d297f8a2a1606057463a822fb9bc36ab2eed
TeamWork <bradyb@dbmaestro.com>
2019-06-04 04:37:54 -0700

    Adding deploy scripts

---
+++ Deploy/0.0.4__Elevation/add_elevation_to_locations.sql
@@ -0,0 +1,12 @@
+alter table locations add (elevation number);
+
+create or replace PROCEDURE "SECURE_DML"
+IS
+BEGIN
+  IF TO_CHAR (SYSDATE, 'HH24:MI') NOT BETWEEN '08:00' AND '18:00'
+        OR TO_CHAR (SYSDATE, 'DY') IN ('SAT', 'SUN') THEN
+	RAISE_APPLICATION_ERROR (-20205,
+		'You may only make changes during normal office hours, dummy');
+  END IF;
+END secure_dml;
+/
